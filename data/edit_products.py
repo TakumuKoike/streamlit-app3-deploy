198d442f900233ef46431c94c846b524ae56572e
TakumuKoike <takumu0424tus@gmail.com>
2025-05-11 15:26:36 +0900

    products.csvのstock_status列に'残りわずか'を追加、およびそのように実行pyファイルを更新

--- data/edit_products.py
+++ data/edit_products.py
@@ -6,7 +6,7 @@ df = pd.read_csv("products.csv", encoding="cp932")
 # df = pd.read_csv("products.csv") #このように書いてしまうと文字コードが違ったときにエラーになる
 
 # 列「stock_status」にランダムで「あり」または「なし」を入れる
-df["stock_status"] = [random.choice(["あり", "なし"]) for _ in range(len(df))]
+df["stock_status"] = [random.choice(["あり", "残りわずか","なし"]) for _ in range(len(df))]
 
 # 新しいCSVとして保存（上書きも可）
 df.to_csv("products.csv", index=False, encoding="cp932")
