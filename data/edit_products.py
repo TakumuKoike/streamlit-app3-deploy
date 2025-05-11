76eaf499696bbc296e108bbfc389eaccc433bf5a
TakumuKoike <takumu0424tus@gmail.com>
2025-05-11 15:08:29 +0900

    products.csvのstock_status列にあり/なしを追記。またその実行pyファイルを追加。

---
+++ data/edit_products.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import random
+
+# CSVファイルを読み込む
+df = pd.read_csv("products.csv", encoding="cp932")
+# df = pd.read_csv("products.csv") #このように書いてしまうと文字コードが違ったときにエラーになる
+
+# 列「stock_status」にランダムで「あり」または「なし」を入れる
+df["stock_status"] = [random.choice(["あり", "なし"]) for _ in range(len(df))]
+
+# 新しいCSVとして保存（上書きも可）
+df.to_csv("products.csv", index=False, encoding="cp932")
+# df.to_csv("products.csv", index=False) #このように書いてしまうと文字化けした形で保存されてしまうことがある
